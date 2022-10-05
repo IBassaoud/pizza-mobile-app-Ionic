9eb2b6388f623abc3ed48b3b9f807bdead9a6dc8
IBassaoud <ismail.bassaoud@gmail.com>
2022-10-05 12:12:06 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pizza_app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
